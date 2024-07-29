ebbc66f5660188af300c741444c5e452f8b14ff1
micovi <micleaovidiul@gmail.com>
2024-07-29 12:42:51 +0300

    add payment token process
    add register amm handler and module
    
    add register amm

---
+++ register-amm/register-amm.lua
@@ -0,0 +1,57 @@
+local json = require("json")
+local dexiCore = require("dexi-core.dexi-core")
+
+local register_amm = {}
+
+register_amm.handleRegisterAMM = function(msg)
+    assert(msg.Tags.Quantity, 'Credit notice data must contain a valid quantity')
+    assert(msg.Tags.Sender, 'Credit notice data must contain a valid sender')
+    assert(msg.Tags["X-AMM-Process"], 'Credit notice data must contain a valid amm-process')
+    assert(msg.Tags["X-Token-A"], 'Credit notice data must contain a valid token-a')
+    assert(msg.Tags["X-Token-B"], 'Credit notice data must contain a valid token-b')
+    assert(msg.Tags["X-Name"], 'Credit notice data must contain a valid fee-percentage')
+
+    -- send Register-Subscriber to amm process
+    ao.send({
+      Target = msg.Tags["X-AMM-Process"],
+      Action = "Register-Subscriber",
+      Tags = {
+        ["Subscriber-Process-Id"] = ao.id,
+        ["Owner-Id"] = msg.Tags.Sender,
+        ['Topics'] = json.encode({ "order-confirmation", "liquidity-change" })
+      }
+    })
+
+    -- Pay for the Subscription
+    ao.send({
+      Target = PAYMENT_TOKEN_PROCESS,
+      Action = "Transfer",
+      Tags = {
+        Recipient = msg.Tags["X-AMM-Process"],
+        Quantity = msg.Tags.Quantity,
+        ["X-Action"] = "Pay-For-Subscription"
+      }
+    })
+
+    dexiCore.registerAMM(
+      msg.Tags["X-Name"],
+      msg.Tags["X-AMM-Process"],
+      msg.Tags["X-Token-A"],
+      msg.Tags["X-Token-B"],
+      msg.Timestamp
+    )
+
+    -- send confirmation to sender
+    ao.send({
+      Target = msg.Tags.Sender,
+      Action = "Dexi-AMM-Registration-Confirmation",
+      Tags = {
+        ["AMM-Process"] = msg.Tags["X-AMM-Process"],
+        ["Token-A"] = msg.Tags["X-Token-A"],
+        ["Token-B"] = msg.Tags["X-Token-B"],
+        ["Name"] = msg.Tags["X-Name"]
+      }
+    })
+end
+
+return register_amm
