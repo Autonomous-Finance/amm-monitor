7aaf1f122a0dbb7395c7fc50bb19421d1761da00
dvinubius <dvinubiu.narmon@gmail.com>
2024-08-02 17:57:14 +0300

    fix build setup

---
+++ build-ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Recreate build directories
+rm -rf ./build
+
+mkdir -p ./build
+    
+amalg.lua -s process.lua -o build/output.lua \
+    validation.validation validation.validation-schemas \
+    db.sqlschema db.seed db.utils \
+    subscriptions.subscriptions \
+    register-amm.register-amm \
+    dexi-core.dexi-core \
+    dexi-core.intervals dexi-core.candles dexi-core.stats dexi-core.overview dexi-core.price-around\
+    ingest.ingest \
+    indicators.indicators indicators.calc \
+    top-n.top-n \
+    utils.debug \
+    ownable.ownable \
+    ops.config-ops ops.emergency
