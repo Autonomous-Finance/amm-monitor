453089bb2954630471760b669b5cbb8d811f7cb4
smowden <smowden@users.noreply.github.com>
2024-08-22 11:47:40 +0300

    add missing files

---
+++ dexi-core/usd-price.lua
@@ -0,0 +1,64 @@
+local dbUtils = require("db-utils")
+local json = require("json")
+
+local mod = {}
+
+function mod.getOraclePriceByProcessId(processId)
+    local stmt = db:prepare [[
+      SELECT *
+      FROM oracle_prices
+      WHERE process_id = :process_id
+      ORDER BY last_update DESC
+      LIMIT 1;
+    ]]
+
+    if not stmt then
+        error("Failed to prepare SQL statement: " .. db:errmsg())
+    end
+
+    stmt:bind_names({
+        process_id = processId
+    })
+
+    local row = dbUtils.queryOne(stmt)
+
+    if row then
+        return {
+            process_id = row.process_id,
+            ticker = row.ticker,
+            price = row.price,
+            last_update = row.last_update
+        }
+    else
+        return nil
+    end
+end
+
+function mod.getUsdPriceForToken(processId)
+    local oraclePrice = mod.getOraclePriceByProcessId(processId)
+
+    if not oraclePrice then
+        return nil
+    end
+
+    return oraclePrice.price
+end
+
+function mod.updateUsdPrice(message)
+    -- TODO: add whitelist check for redstone proxy
+    local redstoneResponse = json.decode(message.Data)
+    local arPrice = redstoneResponse['AR']['v']
+
+    local timestamp = math.floor(redstoneResponse['AR']['t'] / 1000) -- Convert from milliseconds to seconds
+    local date = os.date("%Y-%m-%d %H:%M:%S", timestamp)
+
+    -- insert ar price as ao price
+    local stmt = db:prepare([[
+        INSERT OR REPLACE INTO oracle_prices (process_id, ticker, price, last_update) VALUES ('j7w28CJQHYwamMsotkhE7x0aVUggGwrBtdO5-MQ80uU', 'AO', :price, :date);
+    ]])
+
+    stmt:bind_names({ price = arPrice, date = date })
+    dbUtils.execute(stmt)
+end
+
+return mod
---
+++ redstone-proxy.lua
@@ -0,0 +1,21 @@
+local json = require("json")
+
+DEXI_PROCESS = 'snR3flTItDdCtCqHrJBWTV2vg0kj0onq2Wydlu-crhc'
+
+Handlers.add("Cron-Tick", Handlers.utils.hasMatchingTag("Action", "Cron-Tick"), function(message)
+    ao.send({
+        Target = '4fVi8P-xSRWxZ0EE0EpltDe8WJJvcD9QyFXMqfk-1UQ', -- the Oracle Storage Process id
+        Action = "Request-Latest-Data",                         -- 'Request-Latest-Data' can be used for backwards compatibility
+        Tickers = json.encode({ "AR" })                         -- required prices
+    })
+end)
+
+
+Handlers.add("Receive-RedStone-Prices", Handlers.utils.hasMatchingTag("Action", "Receive-RedStone-Prices"),
+    function(message)
+        ao.send({
+            Target = DEXI_PROCESS,
+            Action = "Receive-RedStone-Prices",
+            Data = message.Data
+        })
+    end)
