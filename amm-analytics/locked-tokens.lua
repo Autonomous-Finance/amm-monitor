17a8f36d0fad105f603c0a2741b6cb225fd9a81b
smowden <smowden@users.noreply.github.com>
2024-10-09 18:48:42 +0300

    fix

--- amm-analytics/locked-tokens.lua
+++ amm-analytics/locked-tokens.lua
@@ -1,7 +1,9 @@
 local poolTokenPnl = require("amm-analytics.pool-token-pnl")
 local dbUtils = require("db.utils")
 
-local function getOneYearLockedTokens(ammProcess)
+local mod = {}
+
+function mod.getOneYearLockedTokens(ammProcess)
     local stmt = [[
         SELECT
             SUM(CAST(current_locked_value AS INTEGER)) AS total_locked_tokens
@@ -10,10 +12,10 @@ local function getOneYearLockedTokens(ammProcess)
     ]]
 
     local result = dbUtils.queryOneWithParams(stmt, { amm_process = ammProcess, current_timestamp = os.time() })
-    return result.total_locked_tokens
+    return result and result.total_locked_tokens or 0
 end
 
-local function getAggregateLockedTokens(ammProcess)
+function mod.getAggregateLockedTokens(ammProcess)
     local stmt = [[
         SELECT
             strftime('%Y-%m-%d', ceil(created_at_ts / 1000)) AS date,
@@ -26,8 +28,10 @@ local function getAggregateLockedTokens(ammProcess)
     return dbUtils.query(stmt, { amm_process = ammProcess })
 end
 
-local function getOneYearLockedShare(ammProcess)
-    local lockedAmount = getOneYearLockedTokens(ammProcess)
+function mod.getOneYearLockedShare(ammProcess)
+    local lockedAmount = mod.getOneYearLockedTokens(ammProcess)
     local poolTokenAmount = poolTokenPnl.getLastPoolTokenAmount(ammProcess)
     return lockedAmount / poolTokenAmount
 end
+
+return mod
