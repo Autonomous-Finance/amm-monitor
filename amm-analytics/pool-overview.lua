a414bdbe6d96c322bdbabea9664ecc467800c287
smowden <smowden@users.noreply.github.com>
2024-10-10 11:06:05 +0300

    show tvl for non mockao pools

--- amm-analytics/pool-overview.lua
+++ amm-analytics/pool-overview.lua
@@ -5,18 +5,14 @@ local responses = require('utils.responses')
 local analytics = {}
 
 local overviewQuery = [[
-WITH latest_transactions_for_pool AS (
-    SELECT
-        row_number() over (PARTITION BY amm_process ORDER BY created_at_ts DESC) as seq,
-        amm_process,
-        price,
-        quote_denominator
-    FROM amm_transactions_view
-), tx_counts AS (
+WITH tx_counts AS (
     SELECT amm_process, count(1) cnt FROM amm_transactions_view GROUP BY 1
 ), fees30d AS (
-    SELECT amm_process, SUM(volume * .2) AS fees_in_quote_30d FROM amm_transactions_view WHERE created_at_ts >= :now - 2592000 GROUP BY 1
-), preagg AS (
+    SELECT amm_process, SUM(volume_usd * .25) AS fees_in_usd_30d FROM amm_transactions_view WHERE created_at_ts >= :now - 2592000 GROUP BY 1
+), tvl AS (
+    SELECT amm_process, tvl_in_usd FROM reserve_changes
+),
+preagg AS (
     SELECT
         t1.token_ticker AS base_token_ticker,
         t0.token_ticker AS quote_token_ticker,
@@ -25,15 +21,14 @@ WITH latest_transactions_for_pool AS (
         t0.token_process AS quote_token_process,
         t1.token_process AS base_token_process,
         asp.amm_process AS amm_process,
-        ((CAST(reserves_0 AS REAL) + (CAST(reserves_1 AS REAL) * coalesce(ltfp.price, 0))) / POW(10, quote_denominator)) tvl_in_quote,
-        ((CAST(reserves_0 AS REAL) + (CAST(reserves_1 AS REAL)) * coalesce(ltfp.price, 0)) / POW(10, quote_denominator)) * op.price tvl_in_usd,
-        f30.fees_in_quote_30d / ((CAST(reserves_0 AS REAL) + (CAST(reserves_1 AS REAL) * coalesce(ltfp.price, 0))) / POW(10, quote_denominator)) AS apr_30d,
+        -- ((CAST(reserves_0 AS REAL) + (CAST(reserves_1 AS REAL) * coalesce(ltfp.price, 0))) / POW(10, quote_denominator)) tvl_in_quote,
+        tvl_in_usd,
+        f30.fees_in_usd_30d / tvl_in_usd AS apr_30d,
         tx_counts.cnt AS tx_count
     FROM amm_swap_params asp
-    LEFT JOIN latest_transactions_for_pool ltfp ON ltfp.amm_process = asp.amm_process AND seq = 1
     LEFT JOIN tx_counts USING (amm_process)
+    LEFT JOIN tvl USING (amm_process)
     LEFT JOIN amm_registry ar USING (amm_process)
-    LEFT JOIN oracle_prices op ON op.process_id = ar.amm_token0
     LEFT JOIN fees30d f30 ON f30.amm_process = ar.amm_process
     LEFT JOIN token_registry t0 ON t0.token_process = ar.amm_token0
     LEFT JOIN token_registry t1 ON t1.token_process = ar.amm_token1
